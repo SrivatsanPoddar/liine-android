f20ff126e7706a7f1df17c95a177908a35010b8b
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-06-29 13:24:07 -0400

    Adding in sqlite management

---
+++ src/com/SrivatsanPoddar/helpp/DatabaseHandler.java
@@ -0,0 +1,34 @@
+public class DatabaseHandler extends SQLiteOpenHelper
+{
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "serviceTree";
+    private static final String SERVICE_TABLE = "service";
+    
+    private static final String NODE_ID = "NodeID";
+    private static final String PARENT_ID = "ParentID";
+    private static final String DISP_TEXT = "DisplayText";
+    
+    public DatabaseHandler(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+    //Creating tables
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String CREATE_TABLE = "CREATE TABLE " + SERVICE_TABLE + "(" 
+                + NODE_ID + " INTEGER PRIMARY KEY,"
+                + PARENT_ID + " INTEGER,"
+                + DISP_TEXT + " TEXT" + ")";
+        db.execSQL(CREATE_TABLE);
+    }
+    
+    //Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("DROP TABLE IF EXISTS " + SERVICE_TABLE);
+        onCreate(db);
+    }
+}
--- src/com/SrivatsanPoddar/helpp/Node.java
+++ src/com/SrivatsanPoddar/helpp/Node.java
@@ -9,15 +9,16 @@ public class Node implements Serializable{
 	private int parentNodeId;
 	private String displayText;
 	
-	public Node[] getChildren() {
-		Node[] childrenNodes={new Node(nodeId*2,nodeId,"Support"),new Node(nodeId*2+1,nodeId,"Add Service"),new Node(nodeId*2+2,nodeId,"Remove Services")};
-		return childrenNodes;
-	}
 	public Node(int node_id, int parent_node_id, String display_text) {
 		nodeId = node_id;
 		parentNodeId = parent_node_id;
 		displayText = display_text;
 	}
+	
+	public Node[] getChildren() {
+        Node[] childrenNodes={new Node(nodeId*2,nodeId,"Support"),new Node(nodeId*2+1,nodeId,"Add Service"),new Node(nodeId*2+2,nodeId,"Remove Services")};
+        return childrenNodes;
+    }
 	
 	public int getNodeId(){
 		return nodeId;
@@ -30,5 +31,4 @@ public class Node implements Serializable{
 	public String toString() {
 		return displayText;
 	}
-	
 }
