f1b74a72be95c2bd49efc4e369976dbd660b37b4
Akshay Srivatsan <akshay.srivatsan@gmail.com>
2014-07-03 13:34:12 -0400

    I officially hate eclipse. Finally fixed it...

--- src/com/SrivatsanPoddar/helpp/SearchActivity.java
+++ src/com/SrivatsanPoddar/helpp/SearchActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.appcompat.R;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
